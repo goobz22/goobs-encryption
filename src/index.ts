98ce76f1959b4dff42289e264e46af9f5fa71af9
Matthew Goluba <mkgoluba@outlook.com>
2024-08-25 15:02:35 -0500

    remove unnecessary imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { ClientEncryptionModule } from './client/encryption.client';
 import { ServerEncryptionModule } from './server/encryption.server';
-import type { EncryptedValue, EncryptionConfig, LogLevel, GlobalConfig } from './types';
+import type { LogLevel, GlobalConfig } from './types';
 
 export { ClientEncryptionModule, ServerEncryptionModule };
 
-export type { EncryptedValue, EncryptionConfig, LogLevel, GlobalConfig };
+export type { LogLevel, GlobalConfig };
